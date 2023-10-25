c60013a4c688e6ef46ba9922625b6292182bc5d0
azheng606 <azheng606@gmail.com>
2023-10-25 18:47:39 -0400

    dom

--- dom.js
+++ dom.js
@@ -12,21 +12,22 @@ image:document.querySelector(".image")}
         console.log (DOMSelectors.name.value, DOMSelectors.title.value, DOMSelectors.image.value)
     
     function poker () {
-        DOMSelectors.box.insertAdjacentHTML("afterend", ` <div class="card">
+        DOMSelectors.box.insertAdjacentHTML("beforeend", `
+        <div class="card">
         <div class ="h1"> ${DOMSelectors.name.value}</div>
             <h3 class="card-title"> ${DOMSelectors.title.value}</h3>
-            <img src= ${DOMSelectors.image.value} alt="Snow White" class="card-img">
+            <img src= ${DOMSelectors.image.value} class="card-img">
         </div>`)};
 
     poker ()
     });
     
-        function clearfields ()  {
-        DOMSelectors.name.value = "" //erases what you wrote in the button after submitting it
-        DOMSelectors.title.value = "" 
-        DOMSelectors.image.value = "" 
-        }
-        clearfields ()
+    function clearfields ()  {
+    DOMSelectors.name.value = "" 
+    DOMSelectors.title.value = "" 
+    DOMSelectors.image.value = "" 
+    }
+    clearfields ()
         
 
 
