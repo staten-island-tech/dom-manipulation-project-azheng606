dad31994e8be88445a127f24081abafaab4076dd
azheng606 <azheng606@gmail.com>
2023-10-26 19:13:38 -0400

    hi

--- dom.js
+++ dom.js
@@ -6,8 +6,7 @@ const DOMSelectors = {
     gone: document.querySelector("#gone"),
     name:document.querySelector(".name"),
     title:document.querySelector(".title"),
-image:document.querySelector(".image")
-}
+image:document.querySelector(".image")}
 
     DOMSelectors.form.addEventListener("submit" ,function (event) { 
         event.preventDefault();
@@ -19,22 +18,17 @@ image:document.querySelector(".image")
         <div class ="h1"> ${DOMSelectors.name.value}</div>
             <h3 class="card-title"> ${DOMSelectors.title.value}</h3>
             <img src= ${DOMSelectors.image.value} class="card-img">
-            <button class="remove" type="button">Remove</button>
-        </div>`
-        )}
+        </div>`)}
         poker ()
         clearfields ()
         
         });
+
 
     function clearfields ()  {
         DOMSelectors.name.value = "" 
         DOMSelectors.title.value = "" 
         DOMSelectors.image.value = "" 
         }
-
-       
-
-      
        
 
